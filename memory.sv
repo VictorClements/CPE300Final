2be5a118894a80bb865e304f37da5c98c06545d9
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-12-01 22:49:50 -0800

    Update memory.sv

--- memory.sv
+++ memory.sv
@@ -1,16 +1,26 @@
-module memory();
+module memory(input  logic        clk,
+              input  logic [15:0] address,
+              input  logic [7:0]  writeData,
+              input  logic        writeEnable,
+              output logic [7:0]  readData);
 //for a combined instruction and data memory, we need a few things:
 /*
-for the Instruction memory:
-    - need a read port
-
-for the Data memory:
     - need a read  port
-    - need a read  enable
+    - need a read  enable??? do we tho
     - need a write port
     - need a write enable
 */
 
-logic [7:0] MEM [63:0] //should be [65535:0], but is smaller for testing purposes
+// for the combined instruction and data mem, we should assume that
+//our instructions will be stored in the first 16 values?
+//and any data will be stored in the last 16 values?
+
+  logic [7:0] MEM [31:0] //should be [65535:0], but is smaller for testing purposes
+  $readmemh("memory.txt", MEM);
+
+  assign readData = MEM[address[4:0]];
+
+  always_ff@(posedge clk)
+    if(writeEnable)    MEM[address[4:0]] <= writeData;
 
 endmodule
