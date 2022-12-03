3993aeb1ebb5f9daa74ef09e0dcc4a079d86796b
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-12-03 02:22:07 -0800

    Update register_Enablable.sv

--- register_Enablable.sv
+++ register_Enablable.sv
@@ -1,10 +1,9 @@
 module register_Enablable 
-       #(parameter width = 8)
-        (input  logic             clk, reset, writeEnable,
-         input  logic [width-1:0] writeData,
-         output logic [width-1:0] readData);
+       #(parameter width = 8) // parameter so i dont have to make 70 billion different register modules
+        (input  logic             clk, reset, writeEnable, // input clock, reset, and enable signals
+         input  logic [width-1:0] writeData, // input of register
+         output logic [width-1:0] readData); // output of register
   always_ff @(posedge clk, posedge reset)
-    if(reset)               readData <= 0;
-    else if(writeEnable)    readData <= writeData;
-    //no need for an else here, but I could put else readData <= readData;
+    if(reset)               readData <= 0; // if reset, then register will hold 0
+    else if(writeEnable)    readData <= writeData; // else if enable is high, then read in value at input of register
 endmodule
