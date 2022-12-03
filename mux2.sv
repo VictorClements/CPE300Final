7f3e8205f64ec37a8a5132dc5f7fb78ae30df00b
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-12-03 02:21:26 -0800

    Update mux2.sv

--- mux2.sv
+++ mux2.sv
@@ -1,8 +1,8 @@
 module mux2
-  #(parameter width = 8)
-   (input  logic [width-1:0] d1, d0, 
-    input  logic             s,
-    output logic [width-1:0] y);
+  #(parameter width = 8) // parameter so i dont have to make 70 billion different 2-to-1 mux modules
+   (input  logic [width-1:0] d1, d0, // input signals that the mux is selecting between
+    input  logic             s, // select line of mux
+    output logic [width-1:0] y); // output of mux
 
-   assign y = s ? d1 : d0; 
+   assign y = s ? d1 : d0; //if(s) then y = d1, else y = d0
 endmodule
